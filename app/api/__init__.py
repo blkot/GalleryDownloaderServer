4832bb4c308774a4d9fca50dd526fc8c7c1982ae
Blackout <wangshenhao@hotmail.com>
2025-10-27 23:46:06 +0800

    add notifications api service

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,7 +3,10 @@
 from fastapi import APIRouter
 
 from .downloads import router as downloads_router
+from .notifications import router as notifications_router
 
 api_router = APIRouter()
 api_router.include_router(downloads_router, prefix="/downloads", tags=["downloads"])
+api_router.include_router(notifications_router, tags=["notifications"])
+
 
